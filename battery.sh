34db0532dc506e17a3e7b6928735dcdaa2397e20
Alex Hiller <xandhiller@gmail.com>
2018-10-23 17:25:27 +1100

    Key remapping script -- pulled it out of bashrc.

--- battery.sh
+++ battery.sh
@@ -1,2 +1,9 @@
-upower -i /org/freedesktop/UPower/devices/battery_BAT0
-upower -i /org/freedesktop/UPower/devices/battery_BAT1
+#!/bin/bash
+
+BAT=$(acpi -b | grep -E -o '[0-9][0-9]?%')
+
+# Full and short texts
+echo "Battery: $BAT"
+echo "BAT: $BAT"
+
+exit 0
---
+++ brightness.sh
@@ -0,0 +1,11 @@
+# Tone down brightness
+# xrandr --output eDP-1 --brightness 1.0 # Max colour brightness, [0.0, 1.0]
+
+# Tone down light intensity
+# sudo vim /sys/class/backlight/intel_backlight/brightness 
+
+# Max brightness level comes from: /sys/class/backlight/intel_backlight/max_brightness
+
+
+# Get connected screen names 
+# xrandr -q | grep "connected"
