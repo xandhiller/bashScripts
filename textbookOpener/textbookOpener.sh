bc399706137bcf35c8ae096651ce5f926a6f4227
Alex Hiller <xandhiller@gmail.com>
2018-11-01 15:35:11 +1100

    Fixed textbook opener to not attempt to open empty strings.

--- textbookOpener/textbookOpener.sh
+++ textbookOpener/textbookOpener.sh
@@ -2,5 +2,10 @@
 
 # List the contents of the Textbooks folder but ignore the archive folder.
 choice="$(ls -I Archive ~/Documents/Textbooks | dmenu -l 20)"
-dir="~/Documents/Textbooks/"
-i3-msg exec --no-startup-id okular $dir$choice
+
+# As long as the choice is not NULL then open the textbook.
+if [ "X$choice" != "X" ]
+then
+  dir="~/Documents/Textbooks/"
+  i3-msg exec --no-startup-id okular $dir$choice
+fi
